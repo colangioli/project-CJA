73c24d5b3c41e7ee7e555bbf0812709d68142864
colangioli <YOUR EMAIL ADDRESSisabellafinco@gmail.com>
2014-07-20 11:57:58 -0400

    cnahged numberline

--- mean.py
+++ mean.py
@@ -5,8 +5,8 @@ import sys
 sum = 0
 n = 0
 # This line indicates that your partner has been here.
-for num in sys.stdin:
-   sum += float(num)
+for numberLine in sys.stdin:
+   sum += float(numberLine)
    n += 1
 
 print "the mean is", (sum / n)
